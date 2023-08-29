49d18c1e191f9af3c15dfebb605e697d556f5162
hazelvaq <hvaquero98@gmail.com>
2023-08-29 04:04:08 +0000

    Determined if favorite desserts are on iconic desserts

--- Addition.R
+++ Addition.R
@@ -1,5 +1,7 @@
 library(tidyverse)
 library(rvest)  # use to scrape website content
+library(here)
+library(janitor)
 
 # Check if that data folder exists and creates it if not
 dir.create("data", showWarnings = FALSE)
@@ -16,3 +18,12 @@ dessert_listing <- dessert_elements %>%
   head(.,-3) %>%               # 3 last ones were not desserts 
   rowid_to_column("rank") %>%  # adding a column using the row number as proxy for the rank
   write_csv("data/iconic_desserts.csv") # save it as csv
+## Read csv
+
+iconic_desserts <- read_csv(here("data","iconic_desserts.csv"))
+
+favorite_desserts <- read_csv("favorite_desserts.csv") %>% clean_names()
+
+inner_join(iconic_desserts,favorite_desserts, by = 'dessert')
+
+favorite_desserts <- favorite_desserts %>% rename(dessert = favorite_dessert)
